2ca3f0f0a074031d391dfea0cf05cf7476a5532d
William Pognonec <william.pognonec@gmail.com>
2024-11-30 09:59:10 -0600

    Move some code to abstract todolist class

--- src/views/AbstractTodos.js
+++ src/views/AbstractTodos.js
@@ -1,4 +1,4 @@
-import { TodoList } from "../components/TodoList"
+// import { TodoList } from "../components/TodoList"
 import { TodoForm } from "../components/TodoForm"
 import { el, mount } from "../lib/dom"
 import Todos from "../models/todos"
@@ -6,34 +6,40 @@ import Todos from "../models/todos"
 export default class AbstractTodos {
   constructor(root) {
     this.root = root
+    this.todos = Todos
     this.todoList = el("div.todoList")
-    mount(this.root, this.todoList)
+    this.todoForm = TodoForm()
+    this.addTodoButton = document.querySelector("#addTodoButton")
     this.updateTodoList()
-    this.createModal()
+    this.addEventListeners()
+    mount(this.root, this.todoList)
+    mount(this.root, this.todoForm)
   }
-  updateTodoList() {
-    let todos = Todos.getAll()
-    const element = TodoList(todos)
-    this.todoList.textContent = ""
-    mount(this.todoList, element)
-  }
-  createModal() {
-    const todoForm = TodoForm()
-    const addTodoButton = document.querySelector("#addTodoButton")
-    addTodoButton.addEventListener("click", () => this.showDialog())
-
-    todoForm.addEventListener("submit", (e) => {
+  updateTodoList() {}
+  addEventListeners() {
+    this.todoList.addEventListener("click", (e) => {
+      if (e.target.hasAttribute("checkbox")) {
+        let id = e.target.parentElement.attributes["data-id"].value
+        this.todos.toggleComplete(id)
+        this.updateTodoList()
+      }
+      if (e.target.hasAttribute("delete")) {
+        let id = e.target.parentElement.attributes["data-id"].value
+        this.todos.delete(id)
+        this.updateTodoList()
+      }
+    })
+    this.addTodoButton.addEventListener("click", () =>
+      this.todoForm.showModal()
+    )
+    this.todoForm.addEventListener("submit", (e) => {
       e.preventDefault()
-      const formData = new FormData(todoForm)
+      const formData = new FormData(e.target)
       let todo = {}
       formData.forEach((v, k) => (todo[k] = v))
-      Todos.save(todo)
+      this.todos.save(todo)
       this.updateTodoList()
+      this.todoForm.close()
     })
-    this.root.appendChild(todoForm)
-  }
-  showDialog() {
-    const dialog = document.querySelector("#addTodoDialog")
-    dialog.showModal()
   }
 }
--- src/views/Inbox.js
+++ src/views/Inbox.js
@@ -1,53 +1,11 @@
 import { TodoList } from "../components/TodoList"
-import { TodoForm } from "../components/TodoForm"
-import { el, mount } from "../lib/dom"
-import Todos from "../models/todos"
+import { mount } from "../lib/dom"
+import AbstractTodos from "./AbstractTodos"
 
-export default class Inbox {
-  constructor(root) {
-    this.root = root
-    this.root.textContent = "Inbox"
-    this.todoList = el("div.todoList")
-    this.todoForm = TodoForm()
-    this.addTodoButton = document.querySelector("#addTodoButton")
-    this.updateTodoList()
-    this.addEventListeners()
-    mount(this.root, this.todoList)
-    mount(this.root, this.todoForm)
-  }
+export default class Inbox extends AbstractTodos {
   updateTodoList() {
-    let todos = Todos.getAll()
-    const element = TodoList(todos)
+    const element = TodoList(this.todos.getAll())
     this.todoList.textContent = ""
     mount(this.todoList, element)
-  }
-  showDialog() {
-    const dialog = document.querySelector("#addTodoDialog")
-    dialog.showModal()
-  }
-  addEventListeners() {
-    this.todoList.addEventListener("click", (e) => {
-      if (e.target.hasAttribute("checkbox")) {
-        let id = e.target.parentElement.attributes["data-id"].value
-        Todos.toggleComplete(id)
-        this.updateTodoList()
-      }
-      if (e.target.hasAttribute("delete")) {
-        let id = e.target.parentElement.attributes["data-id"].value
-        Todos.delete(id)
-        this.updateTodoList()
-      }
-    })
-
-    this.addTodoButton.addEventListener("click", () => this.showDialog())
-    this.todoForm.addEventListener("submit", (e) => {
-      e.preventDefault()
-      const formData = new FormData(e.target)
-      let todo = {}
-      formData.forEach((v, k) => (todo[k] = v))
-      Todos.save(todo)
-      this.updateTodoList()
-      this.todoForm.close()
-    })
   }
 }
